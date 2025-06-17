1505f89e28e59703b85c2bf592d2b498d8d9f26f
pjnalls <42269808+pjnalls@users.noreply.github.com>
2025-06-17 14:26:13 -0500

    fix(main): resolve cmd pkg issue; pass args array

--- cmd/platform/main.go
+++ cmd/platform/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"os"
 
-	"github.com/dockestra/command"
+	"github.com/pjnalls/dockestra/pkg/command"
 )
 
 func main() {
 	switch os.Args[1] {
 	case "run":
-		command.Run(os.Args[2:])
+		command.Run(os.Args)
 	default:
 		panic("bad command")
 	}
